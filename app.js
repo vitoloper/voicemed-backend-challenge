902fd89c311cde1b46b7daa4795fdcaf55b1acde
vitoloper <vitoloper@users.noreply.github.com>
2022-03-27 11:54:52 +0200

    refactor: split app and server code

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Module dependencies
+const fastify = require('fastify');
+
+// Load configuration
+const config = require('./config');
+
+/**
+ * @description Fastify instance builder
+ * @param {object} opts - options
+ * @returns {object} Fastify instance
+ */
+function buildFastify(opts = {}) {
+    const app = fastify(opts);
+
+    // Register custom fastify mongoose plugin
+    app.register(require('./plugins/mongoose.plugin'), { dbconfig: config.db })
+        .after(err => {
+            if (err) {
+                app.log.error(err);
+                process.exit(1);
+            }
+        });
+
+    // Register fastify-swagger plugin (OpenAPI documentation)
+    app.register(require('fastify-swagger'), require('./swagger/swagger.config.js'));
+
+    // Register routes
+    app.register(require('./routes/status.routes'));
+
+    return app;
+}
+
+module.exports = buildFastify;
--- server.js
+++ server.js
@@ -3,37 +3,19 @@
 // Load configuration
 const config = require('./config');
 
-// Create a fastify instance
-const fastify = require('fastify')({ logger: config.app.enableLogging });
+// Create server
+const server = require('./app')({ logger: config.app.enableLogging });
 
-fastify.log.info(`Environment configuration: ${config.env}`);
-
-// Register custom mongoose plugin
-fastify.register(require('./plugins/mongoose.plugin'), { dbconfig: config.db })
-    .after(err => {
-        if (err) {
-            fastify.log.error(err);
-            process.exit(1);
-        }
-    });
-
-// Register fastify-swagger (OpenAPI documentation)
-fastify.register(require('fastify-swagger'), require('./swagger/swagger.config.js'));
-
-// Register routes
-fastify.register(require('./routes/status.routes'));
+server.log.info(`Environment configuration: ${config.env}`);
 
 // Start server
 (async () => {
     try {
         // Listen for incoming requests
-        fastify.log.info('Starting server...');
-        await fastify.listen(config.app.port, config.app.host);
+        server.log.info('Starting server...');
+        await server.listen(config.app.port, config.app.host);
     } catch (err) {
-        fastify.log.error(err)
+        server.log.error(err)
         process.exit(1)
     }
 })();
-
-// Export fastify instance for testing purposes
-module.exports = fastify;
