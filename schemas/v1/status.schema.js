ea9cf64c147cc154ffc4a4b3151eb327d3bb0192
vitoloper <vitoloper@users.noreply.github.com>
2022-03-27 15:46:49 +0200

    feat: add response schema validation to HEAD /status v1

--- schemas/v1/status.schema.js
+++ schemas/v1/status.schema.js
@@ -25,7 +25,17 @@ const getStatus = {
 
 const headStatus = {
     description: 'Get database connection status',
-    tags: ['db']
+    tags: ['db'],
+    response: {
+        200: {
+            description: 'Database connection is healthy',
+            type: 'null'
+        },
+        503: {
+            description: 'Database connection is not healthy',
+            type: 'null'
+        }
+    }
 };
 
 module.exports = {
