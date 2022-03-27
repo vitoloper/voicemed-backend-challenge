a7cb3d60821eb8e3d79ed1943b1b16ca26ecf869
vitoloper <vitoloper@users.noreply.github.com>
2022-03-27 12:34:40 +0200

    test: add HEAD request db connection status to integration tests

--- test/integration.status.test.js
+++ test/integration.status.test.js
@@ -7,20 +7,21 @@ const buildFastify = require('../app');
 const mongoose = require('mongoose');
 
 describe('Status Integration', function () {
-    // Build a fastify instance
-    const fastify = buildFastify();
-
-    before(async function () {
-        // Wait for db connection and server listening
-        await fastify.ready();
-    });
-
-    after(async function () {
-        // Wait for server to close
-        await fastify.close();
-    });
-
     describe('GET /api/v1/status', function () {
+        let fastify;
+
+        before(async function () {
+            // Build a fastify instance
+            fastify = buildFastify();
+            // Wait for db connection and server listening
+            await fastify.ready();
+        });
+
+        after(async function () {
+            // Wait for server to close
+            await fastify.close();
+        });
+
         it('should return {"database": "healthy"} and status code 200 when db connection is healthy', async function () {
             const response = await supertest(fastify.server)
                 .get('/api/v1/status')
@@ -39,6 +40,38 @@ describe('Status Integration', function () {
                 .expect(503);
 
             expect(response.body).to.have.a.property('database').to.equal('unhealthy');
+        });
+    });
+
+    describe('HEAD /api/v1/status', function () {
+        let fastify;
+
+        before(async function () {
+            // Build a fastify instance
+            fastify = buildFastify();
+            // Wait for db connection and server listening
+            await fastify.ready();
+        });
+
+        after(async function () {
+            // Wait for server to close
+            await fastify.close();
+        });
+
+        it('should return status code 200 when db connection is healthy', async function () {
+            const response = await supertest(fastify.server)
+                .get('/api/v1/status')
+                .expect(200);
+        });
+
+        it('should return status code 503 when db connection is unhealthy', async function () {
+
+            // Disconnect from database
+            await mongoose.disconnect();
+
+            const response = await supertest(fastify.server)
+                .get('/api/v1/status')
+                .expect(503);
         });
     });
 });
